1b4494ae14919ff42d4ef1d48a9060ea0a89ed61
Idalis Paz <idalispaz@gmail.com>
2020-06-29 06:56:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
         contacts_details_hash.each do |attribute, data|
-        binding.pry
+         binding.pry
     end
     end 
   end
