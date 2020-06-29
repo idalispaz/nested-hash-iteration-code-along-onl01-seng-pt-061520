498e4885b32da56bb7edd8553459ee279b7f7f51
Idalis Paz <idalispaz@gmail.com>
2020-06-29 06:52:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,7 +13,10 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
+   
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+  binding.pry
+end
 end
